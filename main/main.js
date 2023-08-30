9d5fda29d6bf2ea5c673e067887bfbc74f64e3c2
HYJIN\isacc <isacc7224@gmail.com>
2023-08-30 22:25:41 +0900

    no message

---
+++ main/main.js
@@ -0,0 +1,17 @@
+const images = document.querySelectorAll('.main_photo');
+let currentIndex = 0;
+
+function showImage(index) {
+  images[currentIndex].style.opacity = '0';
+  currentIndex = (index + images.length) % images.length;
+  images[currentIndex].style.opacity = '0.4';
+}
+
+function startSlider() {
+  setInterval(() => {
+    showImage(currentIndex + 1);
+  }, 3000); // 이미지가 3초마다 변경됩니다.
+}
+
+showImage(currentIndex);
+startSlider();
