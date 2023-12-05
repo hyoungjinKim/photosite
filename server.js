dd1f9844c36044b9e7d597d3177c17b8c27eb15c
HYJIN\isacc <isacc7224@gmail.com>
2023-12-05 16:07:36 +0900

    no message

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ const express=require('express')
 const mysql= require('mysql2')
 const path = require('path')
 const static = require('serve-static')
-const redis=require('redis')
-const app= express()
-
+const expressSession=require('express-session')
+const cookieParser=require('cookie-parser')
 //db 정보 받아옴
 const dbconfig = require(`./config/dbconfig.json`)
 
-//받아온 데이터 변환
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
-//public폴더를 최상위 폴더로 바꿈
-app.use(express.static("public"));
-app.use('/', static(path.join(__dirname,`public`)));
-
+//서버 생성
+const app= express()
 //포트 번호
 const port= 4003;
 
-const redisClient = redis.createClient(process.env.REDIS_PORT);
-module.exports = redisClient;
+//받아온 데이터 변환
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+//public폴더를 최상위 폴더로 바꿈
+app.use(express.static("router"));
+app.use('/', static(path.join(__dirname,`public`)));
+app.use(cookieParser());
+app.use(
+    expressSession({
+      secret: "my key",
+      name:'uniqueSessionId',
+      resave: true,
+      saveUninitialized: true,
+    })
+);
 
 //db pool로 생성
 const pool = mysql.createPool({
@@ -37,7 +44,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
 
-app.post('/adduser', async(req,res)=>{
+//회원가입
+app.post('/process/adduser', async(req,res)=>{
     console.log(`/adduser 호출됨${req}`);
 
     const paramid=req.body.id;
@@ -61,6 +69,73 @@ app.post('/adduser', async(req,res)=>{
         res.end();
     }
 })
+
+app.post('/process/login' , async(req,res)=>{
+    console.log('process/login req'+req);
+    const paramid = req.body.id;
+    const parampassword = req.body.password;
+    console.log(`로그인 요청${paramid, parampassword}`);
+
+    await pool.getConnection((err,con)=>{
+        con.release();
+        if(err){
+            console.log('sql run err');
+            console.dir(err);
+            res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
+            res.write('<h2>sql quary 실행 실패</h2>')
+            res.end();
+            return;
+        }
+        const exec = con.query("select `id`, `pw` from `user` where `id`=? and `pw`= SHA2(?,256);",
+            [paramid,parampassword],
+            (err,rows)=>{
+                con.release();
+                console.log('실행된 sql quary:'+exec.sql);
+                
+                if(err){
+                    console.log('sql run err');
+                    console.dir(err);
+                    res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
+                    res.redirect('/main.html')
+                    res.end();
+                    return;
+                }
+
+                if(rows.length>0){
+                    console.log(`아이디[${paramid}]찾음`);
+                    if (req.session.users) {
+                        // 세션에 유저가 존재한다면
+                        console.log("이미 로그인 돼있습니다~");
+                        res.status(200).redirect('/main.html')
+                        res.end();
+                      } else {
+                        req.session.users = {
+                          id: paramid,
+                          pw: parampassword,
+                          authorized: true,
+                        };
+                        res.status(200).redirect('/main.html')
+                        res.end();
+                      };                   
+                    res.end();
+                    return;
+                }
+                else{
+                    console.log(`아이디와 패스워드 일치 안함`);
+                    res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
+                    res.write('<h2>login faile</h2>')
+                    res.end();
+                    return;
+                }
+            }
+        )
+    })
+})
+
+
+
+
+
 
 
 app.listen(port, function(err){
