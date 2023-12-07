ea162f75e3f1b2b90142ed16ec65a693c6834f4e
HYJIN\isacc <isacc7224@gmail.com>
2023-12-08 04:17:52 +0900

    no message

--- router/header/header.js
+++ router/header/header.js
@@ -6,15 +6,22 @@ if(login != 1)
     sessionStorage.setItem("login",0);
 }
 if (login ==1) {
-  nav.innerHTML = `
-    <span>nickname</span>
-    <button class="login_btn" onclick="logout_btn()">로그아웃</button>
+  fetch("/process/nickname",{
+    method: "get",
+  }).then((res)=>res.text())
+    .then((data)=>{
+      nav.innerHTML = `
+    <span>${data}</span>
+    <button class="login_btn" onclick="logout_btn()"><a href="/logout">로그아웃</a></button>
     <button class="membership_btn"><a href="mypictures.html">마이페이지</a></button>`;
+    }).catch((err)=>{
+      console.log(err);
+    })
+  
 }
 
 function logout_btn(){
   sessionStorage.removeItem("login");
-  window.location.href = "main.html";
   nav.innerHTML = `<button class="login_btn"><a href="login.html">로그인</a></button>
   <button class="membership_btn"><a href="membership.html">회원가입</a></button>`;
 }
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const mysql= require('mysql2')
 const path = require('path')
 const static = require('serve-static')
 const expressSession=require('express-session')
-const cookieParser=require('cookie-parser')
-//db 정보 받아옴
-const dbconfig = require(`./config/dbconfig.json`)
 
+//db 정보 받아옴
+const dbconfig = require(`./config/dbconfig.json`);
+//session 정보
+const session =require(`./config/session.json`);
+const { write } = require('fs')
 //서버 생성
 const app= express()
 //포트 번호
@@ -19,14 +21,17 @@ app.use(express.json())
 //public폴더를 최상위 폴더로 바꿈
 app.use(express.static("router"));
 app.use('/', static(path.join(__dirname,`public`)));
-app.use(cookieParser());
+
+//express-session
 app.use(
     expressSession({
-      secret: "my key",
-      name:'uniqueSessionId',
-      resave: true,
-      saveUninitialized: true,
-    })
+      secret: session.key,//암호화에 사용될 키
+      name: session.name,//session 이름
+      resave: false,//세션을 언제나 저장할지 설정
+      saveUninitialized: false,//세션에 저장할 내역이 없더라도 처음부터 세션을 생성할지 설정
+      cookie : {//세션 쿠키 설정
+                //httpOnly: true//js로 세션 쿠키를 사용할수 없도록 함
+    }})
 );
 
 //db pool로 생성
@@ -43,6 +48,7 @@ const pool = mysql.createPool({
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
+
 
 //회원가입
 app.post('/process/adduser', async(req,res)=>{
@@ -70,7 +76,8 @@ app.post('/process/adduser', async(req,res)=>{
     }
 })
 
-app.post('/process/login' , async(req,res)=>{
+//login
+app.post('/login' , async(req,res)=>{
     console.log('process/login req'+req);
     const paramid = req.body.id;
     const parampassword = req.body.password;
@@ -81,12 +88,11 @@ app.post('/process/login' , async(req,res)=>{
         if(err){
             console.log('sql run err');
             console.dir(err);
-            res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
-            res.write('<h2>sql quary 실행 실패</h2>')
+            res.status('500').send('<h2>sql quary 실행 실패</h2>');
             res.end();
             return;
         }
-        const exec = con.query("select `id`, `pw` from `user` where `id`=? and `pw`= SHA2(?,256);",
+        const exec = con.query("select * from photo.user where id=? and pw= SHA2(?,256);",
             [paramid,parampassword],
             (err,rows)=>{
                 con.release();
@@ -95,8 +101,7 @@ app.post('/process/login' , async(req,res)=>{
                 if(err){
                     console.log('sql run err');
                     console.dir(err);
-                    res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
-                    res.redirect('/main.html')
+                    res.status(500).redirect('/main.html');
                     res.end();
                     return;
                 }
@@ -105,25 +110,48 @@ app.post('/process/login' , async(req,res)=>{
                     console.log(`아이디[${paramid}]찾음`);
                     if (req.session.users) {
                         // 세션에 유저가 존재한다면
+                        if(req.session.users.name != rows[0].nickname)
+                        {
+                            req.session.destroy((err) => {
+                                if (err) {
+                                  console.log("세션 삭제시에 에러가 발생했습니다.");
+                                  res.writeHead('500').write(`<h2>session delete err</h2>`)
+                                  return;
+                                }
+                                console.log("세션이 삭제됐습니다.");
+                                res.redirect("/login.html");
+                              });
+                        }
                         console.log("이미 로그인 돼있습니다~");
-                        res.status(200).redirect('/main.html')
+                        res.status(200).redirect('/main.html');
                         res.end();
                       } else {
                         req.session.users = {
+                          Logined: true,
                           id: paramid,
                           pw: parampassword,
+                          name :rows[0].nickname,
                           authorized: true,
                         };
-                        res.status(200).redirect('/main.html')
+                        console.log(req.session.users.name);
+                        res.status(200).redirect('/main.html');
                         res.end();
                       };                   
                     res.end();
                     return;
                 }
                 else{
+                    if(req.session.users){
+                         req.session.destroy((err) => {
+                        if (err) {
+                          console.log("세션 삭제시에 에러가 발생했습니다.");
+                          return;
+                        }
+                        console.log("세션이 삭제됐습니다.");
+                      });
+                    }   
                     console.log(`아이디와 패스워드 일치 안함`);
-                    res.writeHead('500',{'Content-Type':'text/html; charset=utf8'})
-                    res.write('<h2>login faile</h2>')
+                    res.status('500').send(`아이디와 패스워드 일치 안함<h2><a href="/login.html">이동<</a>/h2>`);
                     res.end();
                     return;
                 }
@@ -131,12 +159,44 @@ app.post('/process/login' , async(req,res)=>{
         )
     })
 })
+//닉네임 가져오기
+app.get("/process/nickname", async(req,res)=>{
+    if(req.session.users){
+        console.log("닉네임 전송 성공");
+        console.log(req.session.users.name);
+        res.send(req.session.users.name);
+    }else{
+        await req.session.destroy((err) => {
+            if (err) {
+              console.log("세션 삭제시에 에러가 발생했습니다.");
+              res.writeHead('500').write(`<h2>session delete err</h2>`)
+              return;
+            }
+            console.log("세션이 삭제됐습니다.");
+            res.redirect("/login.html");
+          });
+    }
+})
 
-
-
-
-
-
+//logout
+app.get("/logout", async(req, res) => {
+    console.log("로그아웃");
+    console.log(req.session.users);
+    if (req.session.users) {
+      console.log("로그아웃중입니다!");
+      await req.session.destroy((err) => {
+        if (err) {
+          console.log("세션 삭제시에 에러가 발생했습니다.");
+          return;
+        }
+        console.log("세션이 삭제됐습니다.");
+        res.redirect("/main.html");
+      });
+    } else {
+      console.log("로그인이 안돼있으시네요?");
+      res.status(302).redirect("/main.html");
+    }
+});
 
 app.listen(port, function(err){
     if(err) return console.log(err);
