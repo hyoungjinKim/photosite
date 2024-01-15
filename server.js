b4ee8aa1459566461fdfb9acd85579ca175c7a23
HYJIN\isacc <isacc7224@gmail.com>
2024-01-15 13:24:40 +0900

    no message

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const cookie=require('cookie-parser');
 const fs = require('fs');
 const multer = require('multer');
 
+
+const app= express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs'); //view engine이 사용할 Template Engine
+
 const storage=multer.diskStorage({
   destination: function(req,file,cd){
     cd(null,"./public/images/");
@@ -25,7 +31,6 @@ const dbconfig = require(`./config/dbconfig.json`);
 const session =require(`./config/session.json`);
 
 //서버 생성
-const app= express();
 //포트 번호
 const port= 4003;
 
@@ -33,7 +38,7 @@ const port= 4003;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //public폴더를 최상위 폴더로 바꿈
-app.use('/', static(path.join(__dirname,`public`)));
+app.use(static(path.join(__dirname,`/public`)));
 app.use(cookie());
 
 //express-session
@@ -59,10 +64,158 @@ const pool = mysql.createPool({
     debug:false
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+const setHeaderData = (req, res, next) => {
+  // 사용자가 로그인한 경우
+  if (req.session.users && req.session.users.Logined) {
+    res.locals.name = req.session.users.name;
+  } else {
+    // 로그인하지 않은 경우
+    res.locals.name = '';
+  }
+  next();
+};
+
+app.use(setHeaderData);
+
+app.get('/', async(req, res) => {
+  await pool.getConnection((err,con)=>{
+    if(err){
+      console.log('sql run err');
+      console.dir(err);
+      res.status('500').redirect('/main.ejs');
+      res.end();
+    }else{
+      con.query("select * from photo.photo;",
+      (err,rows)=>{
+        con.release();
+        if(err){
+          console.log('sql run err');
+          console.dir(err);
+          res.status(500).redirect('/main.ejs');
+          res.end();
+          return;
+        }
+        res.render("main.ejs",{
+          photo: rows
+        })
+      })
+    }
+  })
 });
 
+app.get('/login.ejs', (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get('/membership.ejs', (req, res) => {
+  res.render("membership.ejs");
+});
+
+app.get('/loginerr.ejs', (req, res) => {
+  res.render("loginerr.ejs");
+});
+
+app.get('/scrap.ejs', (req, res) => {
+  res.render("scrap.ejs");
+});
+
+app.get('/search.ejs', (req, res) => {
+  res.render("search.ejs");
+});
+
+app.get('/photos.ejs', async (req, res) => {
+  await pool.getConnection((err,con)=>{
+    if(err){
+        console.log('sql run err');
+        console.dir(err);
+        res.status('500').redirect('/main.ejs');
+        res.end();
+    }
+    con.query("select * from photo.photo;",
+        (err,rows)=>{
+            con.release();
+            if(err){
+                console.log('sql run err');
+                console.dir(err);
+                res.status(500).redirect('/main.ejs');
+                res.end();
+                return;
+            }
+            if (rows.length > 0) {
+              res.render("photos.ejs", {
+                title: `${rows[0].title}`,
+                comment: `${rows[0].comment}`
+              });
+              con.release();
+            }
+          }
+        )
+    })
+});
+
+app.get('/mypictures.ejs', async (req, res) => {
+  if (req.session.users.Logined) {
+    await pool.getConnection((err, con) => {
+      if (err) {
+        console.log('sql run err');
+        console.dir(err);
+        res.status(500).redirect('/main.ejs');
+        res.end();
+      }
+      con.query(`select * from photo.photo where user_id=?;`,
+        [req.session.users.id],
+        (err, rows) => {
+          con.release();
+          if (err) {
+            console.log('sql run err');
+            console.dir(err);
+            res.status(500).redirect('/main.ejs');
+            res.end();
+            return;
+          }
+          console.log(rows);
+          res.render("mypictures.ejs", {
+            photo: rows,
+          });
+        }
+      );
+    });
+  } else {
+    console.log(`로그인하세요`);
+    res.redirect('/login.ejs');
+  }
+});
+
+
+app.get('/main.ejs', async(req, res) => {
+  await pool.getConnection((err,con)=>{
+    if(err){
+      console.log('sql run err');
+      console.dir(err);
+      res.status('500').redirect('/main.ejs');
+      res.end();
+    }else{
+      con.query("select * from photo.photo;",
+      (err,rows)=>{
+        con.release();
+        if(err){
+          console.log('sql run err');
+          console.dir(err);
+          res.status(500).redirect('/main.ejs');
+          res.end();
+          return;
+        }
+        res.render("main.ejs",{
+          photo: rows
+        })
+      })
+    }
+  })
+});
+
+app.get('/photoupload.ejs', (req, res) => {
+  res.render("photoupload.ejs");
+});
 
 //회원가입
 app.post('/process/adduser', async(req,res)=>{
@@ -75,13 +228,12 @@ app.post('/process/adduser', async(req,res)=>{
         const con = await pool.promise().getConnection();
 
         console.log('connect db');
-        const result =await con.query('INSERT INTO user (id, pw, nickname) VALUES (?, SHA2(?,256), ?);',
+        await con.query('INSERT INTO user (id, pw, nickname) VALUES (?, SHA2(?,256), ?);',
         [paramid,parampassword,paramnickname]);
     con.release();
 
-    console.dir(result);
     console.log('insert success');
-    res.redirect('/login.html');
+    res.redirect('/login.ejs');
     } catch(err){
         console.error('sql run err');
         console.error(err);
@@ -106,16 +258,15 @@ app.post('/login' , async(req,res)=>{
             res.end();
             return;
         }
-        const exec = con.query("select * from photo.user where id=? and pw= SHA2(?,256);",
+        con.query("select * from photo.user where id=? and pw= SHA2(?,256);",
             [paramid,parampassword],
             (err,rows)=>{
                 con.release();
-                console.log('실행된 sql quary:'+exec.sql);
                 
                 if(err){
                     console.log('sql run err');
                     console.dir(err);
-                    res.status(500).redirect('/main.html');
+                    res.status(500).redirect('/main.ejs');
                     res.end();
                     return;
                 }
@@ -133,11 +284,11 @@ app.post('/login' , async(req,res)=>{
                                   return;
                                 }
                                 console.log("세션이 삭제됐습니다.");
-                                res.redirect("/login.html");
+                                res.redirect("/login.ejs");
                               });
                         }
                         console.log("이미 로그인 돼있습니다~");
-                        res.status(200).redirect('/main.html');
+                        res.status(200).redirect('/main.ejs');
                         res.end();
                       } else {
                         req.session.users = {
@@ -147,7 +298,7 @@ app.post('/login' , async(req,res)=>{
                           authorized: true,
                         };
                         console.log(req.session.users.name);
-                        res.status(200).redirect('/main.html');
+                        res.status(200).redirect('/main.ejs');
                         res.end();
                       };                   
                     res.end();
@@ -164,7 +315,7 @@ app.post('/login' , async(req,res)=>{
                       });
                     }   
                     console.log(`아이디와 패스워드 일치 안함`);
-                    res.status(500).redirect('/loginerr.html');
+                    res.status(500).redirect('/loginerr.ejs');
                     res.end();
                     return;
                 }
@@ -183,22 +334,22 @@ app.get("/process/nickname", async(req,res)=>{
 })
 
 //logout
-app.get("/logout", async(req, res) => {
+app.get("/logout", (req, res) => {
     console.log("로그아웃");
     console.log(req.session.users);
     if (req.session.users) {
       console.log("로그아웃중입니다!");
-      await req.session.destroy((err) => {
+      req.session.destroy((err) => {
         if (err) {
           console.log("세션 삭제시에 에러가 발생했습니다.");
           return;
         }
         console.log("세션이 삭제됐습니다.3");
-        res.redirect("/main.html");
+        res.redirect("/main.ejs");
       });
     } else {
       console.log("로그인이 안돼있으시네요?");
-      res.status(302).redirect("/main.html");
+      res.status(302).redirect("/main.ejs");
     }
 });
 
@@ -222,21 +373,29 @@ app.post('/upload', upload.single('photo'), async (req, res) => {
       const userid=  req.session.users.id;
       
       // 이미지 삽입
-      const imgInput = await con.query('INSERT INTO photo (img_no, img, title, comment, user_id) VALUES (?, ?, ?, ?, ?);',
+      await con.query('INSERT INTO photo (img_no, img, title, comment, user_id) VALUES (?, ?, ?, ?, ?);',
         [count + 1, image , title, comment, userid]);
       con.release();
       
       //tag 삽입
-      for(let i=0; i<selectedTags.length; i++){
-        console.log(selectedTags[i]);
-
-        let tagInput = con.query('INSERT INTO tag (img_no, tag) VALUES (?, ?);',
-        [count+1, selectedTags[i]]);
+      if(Array.isArray(selectedTags))
+      {
+        selectedTags.forEach(element => {
+          console.log(element);
+          
+          let tagInput = con.query('INSERT INTO tag (img_no, tag) VALUES (?, ?);',
+          [count+1, element]);
+        });
+      }else{
+        console.log(selectedTags);
+        con.query('INSERT INTO tag (img_no, tag) VALUES (?, ?);',
+          [count+1, selectedTags]);
       }
+      
       
 
       console.log('Insert success');
-      res.status(500).redirect('/photos.html');
+      res.status(500).redirect('/photos.ejs');
     }
   } catch (error) {
     console.error(error);
